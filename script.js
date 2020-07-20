cc23a26f344850f8a865a239248a278be5289072
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2020-07-20 16:23:02 -0700

    Added a debug message and code to default to 4 images if more than 4 images are uploaded

--- script.js
+++ script.js
@@ -47,25 +47,37 @@ function findSelectedBackground () {
   generateCollage generates the picture collage using the array of uploaded `images` and by overlaying them on the `background` image.
 */
 function generateCollage (images, background) {
-  var totalImages = images.length
-  var url = 'https://res.cloudinary.com/' + window.cloudName + '/image/upload/'
+  // this div will hold the result.
+  var collage = document.getElementById('finalCollage')
 
-  if (totalImages === 2) {
-    url += '$h_ih_sub_20,$w_iw_div_2_sub_20,$x_iw_div_4,$x1_iw_div_4_mul_-1,$nw_iw,$nh_ih_div_2,$t_ih_div_4/c_scale,w_$nw,h_$nh' +
+  var totalImages = images.length
+  // set the number of images to be a max of 4
+  if (totalImages > 4) {
+    totalImages = 4
+  }
+
+  // display an error message if there are less than 4 images
+  if (totalImages < 2) {
+    collage.innerHTML = "<span style='color:red'>Please choose at least 2 images to create a collage!</span>"
+  } else {
+    var url = 'https://res.cloudinary.com/' + window.cloudName + '/image/upload/'
+
+    if (totalImages === 2) {
+      url += '$h_ih_sub_20,$w_iw_div_2_sub_20,$x_iw_div_4,$x1_iw_div_4_mul_-1,$nw_iw,$nh_ih_div_2,$t_ih_div_4/c_scale,w_$nw,h_$nh' +
               '/l_' + images[0] +
               ',w_$w,h_$h,c_pad,x_$x,e_unsharp_mask' +
               '/l_' + images[1] +
               ',w_$w,h_$h,c_pad,x_$x1,e_unsharp_mask/'
-  } else if (totalImages === 3) {
-    url += '$ox_iw_div_4,$ox1_iw_div_4_mul_-1,$oy_ih_div_4,$oy1_ih_div_4_mul_-1,$w_iw_div_2_sub_20,$w1_iw_sub_20,$h_ih_div_2_sub_20,$t_ih_div_4/' +
+    } else if (totalImages === 3) {
+      url += '$ox_iw_div_4,$ox1_iw_div_4_mul_-1,$oy_ih_div_4,$oy1_ih_div_4_mul_-1,$w_iw_div_2_sub_20,$w1_iw_sub_20,$h_ih_div_2_sub_20,$t_ih_div_4/' +
         'l_' + images[0] +
         ',x_$ox,y_$oy1,w_$w,h_$h,c_pad,e_unsharp_mask/' +
         'l_' + images[1] +
         ',x_$ox1,y_$oy1,w_$w,h_$h,c_pad,e_unsharp_mask/' +
         'l_' + images[2] +
         ',y_$oy,w_$w,h_$h,c_pad,e_unsharp_mask/'
-  } else { // if (totalImages === 4)
-    url += '$ox_iw_div_4,$ox1_iw_div_4_mul_-1,$oy_ih_div_4,$oy1_ih_div_4_mul_-1,$w_iw_div_2_sub_20,$h_ih_div_2_sub_20,$t_ih_div_4/' +
+    } else if (totalImages === 4) {
+      url += '$ox_iw_div_4,$ox1_iw_div_4_mul_-1,$oy_ih_div_4,$oy1_ih_div_4_mul_-1,$w_iw_div_2_sub_20,$h_ih_div_2_sub_20,$t_ih_div_4/' +
         'l_' + images[0] +
         ',x_$ox,y_$oy1,w_$w,h_$h,c_pad,e_unsharp_mask/' +
         'l_' + images[1] +
@@ -74,22 +86,24 @@ function generateCollage (images, background) {
         ',x_$ox,y_$oy,w_$w,h_$h,c_pad,e_unsharp_mask/' +
         'l_' + images[3] +
         ',x_$ox1,y_$oy,w_$w,h_$h,c_pad,e_unsharp_mask/'
+    }
+
+    if (totalImages >= 2 && totalImages <= 4) {
+      if (document.getElementById('message').value) {
+        url += '/l_text:Sacramento_35_bold:' + encodeURI(document.getElementById('message').value) + ',y_$t,co_rgb:990C47,g_north/'
+      }
+      url += 'f_auto,q_auto/' + background
+
+      var img = new Image()
+      img.src = url
+      collage.append(img)
+
+      // add a get the collage link
+      var ahref = document.createElement('a')
+      ahref.href = url.replace('f_auto', 'fl_attachment')
+      ahref.target = '_blank'
+      ahref.text = 'Download the image'
+      collage.append(ahref)
+    }
   }
-
-  if (document.getElementById('message').value) {
-    url += '/l_text:Sacramento_35_bold:' + encodeURI(document.getElementById('message').value) + ',y_$t,co_rgb:990C47,g_north/'
-  }
-  url += 'f_auto,q_auto/' + background
-
-  var img = new Image()
-  img.src = url
-  var collage = document.getElementById('finalCollage')
-  collage.append(img)
-
-  // add a get the collage link
-  var ahref = document.createElement('a')
-  ahref.href = url.replace('f_auto', 'fl_attachment')
-  ahref.target = '_blank'
-  ahref.text = 'Download the image'
-  collage.append(ahref)
 }
